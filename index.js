f52833cbeec00de53ffb0c3e5d7b410fb0a07a91
Restyled.io <commits@restyled.io>
2019-12-23 05:40:40 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const { name } = require('./package');
+const {name} = require('./package');
 const FastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name,
-  options: {
-    nodeAssets: {
-      'opus-recorder': {
-        public: {
-          srcDir: 'dist',
-          destDir: 'opus-recorder',
-          include: ['waveWorker.min.js', 'encoderWorker.min.js', 'encoderWorker.min.wasm'],
-          processTree(input) {
-            return FastbootTransform(input);
-          }
+  options : {
+    nodeAssets : {
+      'opus-recorder' : {
+        public : {
+          srcDir : 'dist',
+          destDir : 'opus-recorder',
+          include : [
+            'waveWorker.min.js', 'encoderWorker.min.js',
+            'encoderWorker.min.wasm'
+          ],
+          processTree(input) { return FastbootTransform(input); }
         }
       }
     }
